3835780c4dc144187b7f68579b9dc041f4688878
vrinsidesai <155213468+vrinsidesai@users.noreply.github.com>
2024-01-11 09:03:37 +0530

    Add files via upload

---
+++ mathematical func.php
@@ -0,0 +1,32 @@
+//built in function
+<?php
+
+
+$a=sqrt(16);
+echo $a,'<br>';
+
+$b=rand(1,100);
+echo $b,'<br>';
+
+$c=round(3.7);
+echo $c,'<br>';
+
+$d=ceil(3.2);
+echo $d,'<br>';
+
+$e=floor(3.8);
+echo $e,'<br>';
+
+$f=pow(2,3);
+echo $f,'<br>';
+
+$g=max(5,8,3);
+echo $g,'<br>';
+
+$h=min(5,8,3);
+echo $h,'<br>';
+
+//string function
+ echo strlen("hello,world");
+ 
+?>
